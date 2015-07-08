817ff4d7b3d629ddc48116de67739531fd8499ca
cat12079801 <cat12079801@gmail.com>
2015-07-08 17:39:06 +0900

    admin controllerとuser model作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704022600) do
+ActiveRecord::Schema.define(version: 20150708083355) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20150704022600) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
