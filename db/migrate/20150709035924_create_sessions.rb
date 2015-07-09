48b00feae3766cd6e4e175742c3946e99393fc09
cat12079801 <cat12079801@gmail.com>
2015-07-09 14:11:45 +0900

    migrateフィアル更新(やっちゃダメ)

--- db/migrate/20150709035924_create_sessions.rb
+++ db/migrate/20150709035924_create_sessions.rb
@@ -3,8 +3,6 @@ class CreateSessions < ActiveRecord::Migration
     create_table :sessions do |t|
       t.belongs_to :user, index: true, null: false
       t.string     :sha256_cookie, null: false
-      t.timestamp  :create_session, null: false
-      t.timestamp  :last_request, null: false
       t.boolean    :finish, null: false, default: false
       t.string     :remote_ip, null: false
 
