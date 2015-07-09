c482a11ae9217b84876773bdb70a18d63bc86fa8
cat12079801 <cat12079801@gmail.com>
2015-07-09 22:50:02 +0900

    authorコントローラ、ビュー追加

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -1,10 +1,10 @@
 class Admin::AuthorsController < ApplicationController
-  before_action :set_admin_author, only: [:show, :edit, :update, :destroy]
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/authors
   # GET /admin/authors.json
   def index
-    @admin_authors = Admin::Author.all
+    @authors = Author.all
   end
 
   # GET /admin/authors/1
@@ -14,7 +14,7 @@ class Admin::AuthorsController < ApplicationController
 
   # GET /admin/authors/new
   def new
-    @admin_author = Admin::Author.new
+    @author = Author.new
   end
 
   # GET /admin/authors/1/edit
@@ -24,15 +24,15 @@ class Admin::AuthorsController < ApplicationController
   # POST /admin/authors
   # POST /admin/authors.json
   def create
-    @admin_author = Admin::Author.new(admin_author_params)
+    @author = Author.new(author_params)
 
     respond_to do |format|
-      if @admin_author.save
-        format.html { redirect_to @admin_author, notice: 'Author was successfully created.' }
-        format.json { render :show, status: :created, location: @admin_author }
+      if @author.save
+        format.html { redirect_to [:admin, @author], notice: 'Author was successfully created.' }
+        format.json { render :show, status: :created, location: @author }
       else
         format.html { render :new }
-        format.json { render json: @admin_author.errors, status: :unprocessable_entity }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -41,12 +41,12 @@ class Admin::AuthorsController < ApplicationController
   # PATCH/PUT /admin/authors/1.json
   def update
     respond_to do |format|
-      if @admin_author.update(admin_author_params)
-        format.html { redirect_to @admin_author, notice: 'Author was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_author }
+      if @author.update(author_params)
+        format.html { redirect_to [:admin, @author], notice: 'Author was successfully updated.' }
+        format.json { render :show, status: :ok, location: @author }
       else
         format.html { render :edit }
-        format.json { render json: @admin_author.errors, status: :unprocessable_entity }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,7 +54,7 @@ class Admin::AuthorsController < ApplicationController
   # DELETE /admin/authors/1
   # DELETE /admin/authors/1.json
   def destroy
-    @admin_author.destroy
+    @author.destroy
     respond_to do |format|
       format.html { redirect_to admin_authors_url, notice: 'Author was successfully destroyed.' }
       format.json { head :no_content }
@@ -63,12 +63,12 @@ class Admin::AuthorsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_admin_author
-      @admin_author = Admin::Author.find(params[:id])
+    def set_author
+      @author = Author.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def admin_author_params
-      params[:admin_author]
+    def author_params
+      params.require(:author).permit(:name, :account)
     end
 end
