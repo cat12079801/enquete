aa3a840e80e451e65d9226a1b34d3b3401568363
cat12079801 <cat12079801@gmail.com>
2015-07-15 01:59:37 +0900

    アンケート回収モデル追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
   has_many :game_author
   has_many :author, :through => :game_author
+  has_many :collect
 
   mount_uploader :image, PictureUploader
 end
---
+++ db/migrate/20150714165334_create_collects.rb
@@ -0,0 +1,15 @@
+class CreateCollects < ActiveRecord::Migration
+  def change
+    create_table :collects do |t|
+      t.belongs_to :game_id, index: true, null: false
+      t.integer :evaluation
+      t.string :guest_name
+      t.text :impressions
+      t.string :gender
+      t.integer :age
+      t.string :come_from
+
+      t.timestamps null: false
+    end
+  end
+end
